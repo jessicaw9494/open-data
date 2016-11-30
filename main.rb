8c3e3caf4765dc3ba09e42a364c964d814ff99cd
jessicaw9494 <jessicaw9494@hstat.org>
2016-11-30 18:47:13 +0000

    change question

--- main.rb
+++ main.rb
@@ -1133,7 +1133,7 @@ homebase_locations_data= JSON.parse(%q|
 # puts homebase_locations_data["data"]
 
 
-puts "What is your neighborhood?"
+puts "WWhat borough do you want to live in?"
 code_num = gets.chomp
 homebase_locations_data["data"].each do ||
 end
